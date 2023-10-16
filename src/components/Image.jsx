26aa3e522ea652d8d18580ec731255c628352480
akimas <akihiro0923092@gmaol.com>
2023-10-16 15:58:36 +0900

    second

---
+++ src/components/Image.jsx
@@ -0,0 +1,10 @@
+export default function Image() {
+    return (
+      <figure>
+        <img
+          src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+          alt="cute dog!"
+        />
+      </figure>
+    );
+  }
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import Gallery from "./Gallery";
+export default function Main() {
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Gallery />
+        </div>
+      </section>
+    </main>
+  );
+}
