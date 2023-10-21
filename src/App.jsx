56772f28106eaacafeae79c8c816276695aebc9c
akimas <akihiro0923092@gmaol.com>
2023-10-22 03:44:52 +0900

    canged many parts and add any files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Nav from "./components/Nav.jsx";
+
+import styles from "./CssModules.module.css";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Nav />
       <Main />
       <Footer />
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,13 @@
-import Gallery from "./Gallery";
+import WelcomeText from "./WelcomeText.jsx";
+import Gallery from "./Gallery.jsx";
+
 export default function Main() {
   return (
     <main>
       <section className="section">
         <div className="container">
-          <Gallery />
+            <WelcomeText />
+            <Gallery />
         </div>
       </section>
     </main>
