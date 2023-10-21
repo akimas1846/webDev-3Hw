7105d66994ca3d8726e75d5eee3077df74922f84
akimas <akihiro0923092@gmaol.com>
2023-10-22 03:50:27 +0900

    add in Gallery figure

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,8 +4,10 @@ export default function Gallery() {
   return (
     <div className="columns is-vcentered is-multiline">
       <div className="column is-3">
-      <h2>Favourite photos</h2>
-        <Image />
+        <h2>Favourite photos</h2>
+        <figure>
+          <Image />
+        </figure>
       </div>
     </div>
   );
