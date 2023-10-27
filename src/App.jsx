0af07014cb6d1f7084b6d50cfaac25b5b711b873
akimas <akihiro0923092@gmaol.com>
2023-10-27 09:21:22 +0900

    aaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,4 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Nav from "./components/Nav.jsx";
-
-import styles from "./CssModules.module.css";
-
 export default function App() {
-  return (
-    <div>
-      <Header />
-      <Nav />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
+    const name = "Chris";
+    return <p>Player 1: {name}</p>;
+  }
--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header>
-            <h1>Birdwatching</h1>
-            <img src="dove.png" alt="a simple dove logo" />
-        </header>
-    );
-}
